78b9df2ba6da2c9029c9d3c0d4950dab87b92507
Natanael Arndt <arndtn@gmail.com>
2015-02-18 16:05:05 +0100

    Add modification date

---
+++ _plugins/myfilters.rb
@@ -0,0 +1,10 @@
+# from http://stackoverflow.com/a/18233371/414075
+module Jekyll
+    module MyFilters
+        def file_date(input)
+            File.mtime(input)
+        end
+    end
+end
+
+Liquid::Template.register_filter(Jekyll::MyFilters)
